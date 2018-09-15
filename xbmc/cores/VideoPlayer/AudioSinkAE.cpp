af553a435d4831592b3cce8ece6badd34c3fbd1f
Rainer Hochecker <fernetmenta@online.de>
2018-09-15 12:28:45 +0200

    VideoPlayer: create new audiosink if pt stream type changes

--- xbmc/cores/VideoPlayer/AudioSinkAE.cpp
+++ xbmc/cores/VideoPlayer/AudioSinkAE.cpp
@@ -68,6 +68,7 @@ bool CAudioSinkAE::Create(const DVDAudioFrame &audioframe, AVCodecID codec, bool
   m_iBitsPerSample = audioframe.bits_per_sample;
   m_bPassthrough = audioframe.passthrough;
   m_channelLayout = audioframe.format.m_channelLayout;
+  m_dataType = audioframe.format.m_streamInfo.m_type;
 
   return true;
 }
@@ -238,6 +239,10 @@ bool CAudioSinkAE::IsValidFormat(const DVDAudioFrame &audioframe)
       m_sampleRate != audioframe.format.m_sampleRate ||
       m_iBitsPerSample != audioframe.bits_per_sample ||
       m_channelLayout != audioframe.format.m_channelLayout)
+    return false;
+
+  if (m_bPassthrough &&
+      m_dataType != audioframe.format.m_streamInfo.m_type)
     return false;
 
   return true;
